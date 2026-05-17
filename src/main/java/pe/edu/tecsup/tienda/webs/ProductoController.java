10a72ca38002402dc963f223310217af16b596a2
jgomezz <jgomezz@gmail.com>
2026-05-17 11:03:33 -0500

    update

--- src/main/java/pe/edu/tecsup/tienda/webs/ProductoController.java
+++ src/main/java/pe/edu/tecsup/tienda/webs/ProductoController.java
@@ -1,21 +1,31 @@
 package pe.edu.tecsup.tienda.webs;
 
 
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import pe.edu.tecsup.tienda.entities.Producto;
+import pe.edu.tecsup.tienda.services.ProductoService;
+
+import java.util.List;
 
 @Slf4j
 @Controller
+@AllArgsConstructor
 @RequestMapping("/productos")
 public class ProductoController {
+
+    private final ProductoService productoService;
 
     @GetMapping()
     public String index(Model model) {
 
-        // TO DO
+        List<Producto> productos = this.productoService.findAll();  // TO DO
+
+        model.addAttribute("productos", productos);
 
         return "/productos/index";
     }
