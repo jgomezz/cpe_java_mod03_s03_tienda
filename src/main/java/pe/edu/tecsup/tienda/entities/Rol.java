e70ad8955c927a1d0f1493d76cd9196920972371
jgomezz <jgomezz@gmail.com>
2026-05-24 09:25:27 -0500

    update

---
+++ src/main/java/pe/edu/tecsup/tienda/entities/Rol.java
@@ -0,0 +1,25 @@
+package pe.edu.tecsup.tienda.entities;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.springframework.security.core.GrantedAuthority;
+
+@Data
+@Entity
+@Table(name="roles")
+public class Rol  implements GrantedAuthority {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nombre;
+
+    // Methodos de GrantedAuthority
+
+    @Override
+    public String getAuthority() {
+        return this.nombre;
+    }
+
+
+}
