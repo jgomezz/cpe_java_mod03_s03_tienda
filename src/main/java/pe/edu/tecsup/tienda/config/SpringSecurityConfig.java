5d1dc35e9bc02b3384aa929bf8218bac0ae2c31c
jgomezz <jgomezz@gmail.com>
2026-05-24 11:42:26 -0500

    add  authorize Spring Security

--- src/main/java/pe/edu/tecsup/tienda/config/SpringSecurityConfig.java
+++ src/main/java/pe/edu/tecsup/tienda/config/SpringSecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -11,6 +12,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -49,6 +51,41 @@ public class SpringSecurityConfig {
     public void configureAuth(AuthenticationManagerBuilder auth)
             throws Exception{
         auth.userDetailsService(userDetailsService);
+    }
+
+    // Autorizacion
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+                // Configuracion de accesos
+                .authorizeHttpRequests((requests) -> requests
+                        .requestMatchers(
+                                "/" ,
+                                "/webjars/**",
+                                "/css/**",
+                                "/error/**").permitAll()
+                        .requestMatchers(
+                                "/productos/**",
+                                "/profile").authenticated())
+                    // Login
+                    .formLogin((form) -> form
+                            .loginProcessingUrl("/authenticate")
+                            .loginPage("/login").permitAll()
+                            .defaultSuccessUrl("/", true)
+                    )
+                    // Logout
+                    .logout((logout) -> logout
+                            .logoutUrl("/logout")
+                            .logoutSuccessUrl("/login")
+                    )
+                    // Change csrf
+                    .csrf( (csrf) -> csrf.disable()
+
+                );
+
+        return http.build();
     }
 
 }
